98c33e72d3c971f34f95ae8f0c501df828d1ffed
Шихалев Дмитрий <yason1234@mail.ru>
2022-07-08 19:43:02 +0300

    add Navigation_HW_1.3

---
+++ Navigation_HW_1.3/Navigation_HW_1.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation_HW_1.3
+//
+//  Created by Dima Shikhalev on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
