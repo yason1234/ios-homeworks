d5a7cb04e085007f8cfab2c22b47ce37e87fe38c
Шихалев Дмитрий <yason1234@mail.ru>
2022-07-31 18:15:23 +0300

    Homework TableView

--- Navigation_HW_1.3/Navigation_HW_1.3/Profile/ProfileViewController.swift
+++ Navigation_HW_1.3/Navigation_HW_1.3/Profile/ProfileViewController.swift
@@ -8,31 +8,112 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
-    private lazy var profileHeaderView = ProfileHeaderView()
     
+    private lazy var myTableView = UITableView(frame: .zero, style: .grouped)
+    private lazy var model: [NewPost] = postArray
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupViews()
+        setUpViews()
+        configure()
         setConstraints()
     }
-        
-    private func setupViews() {
-        
-        view.backgroundColor = .lightGray
-        view.addSubview(profileHeaderView)
-        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+
+    private func setUpViews() {
+
+        view.addSubview(myTableView)
+    }
+
+    private func configure() {
+
+        view.backgroundColor = .white
+        myTableView.translatesAutoresizingMaskIntoConstraints = false
+        myTableView.backgroundColor = .white
+        myTableView.register(PostTableViewCell.self, forCellReuseIdentifier: "customCell")
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        myTableView.rowHeight = UITableView.automaticDimension
+        myTableView.register(ProfileHeaderView.self, forHeaderFooterViewReuseIdentifier: "Header")
     }
 }
 
-extension ProfileViewController {
-    
-    private func setConstraints() {
-        
-        NSLayoutConstraint.activate([
-            profileHeaderView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            profileHeaderView.heightAnchor.constraint(equalToConstant: 350)])
+// MARK: tableView Data and Delegate
+extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        model.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! PostTableViewCell
+        cell.configureViews(authorNew: model[indexPath.row].author, avatar: model[indexPath.row].image, description: model[indexPath.row].description, likes: model[indexPath.row].likes, views: model[indexPath.row].views)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "Header") as! ProfileHeaderView
+        header.delegate = self
+        header.setDelegate()
+        return header
     }
 }
+
+// MARK: constraints
+extension ProfileViewController {
+
+    private func setConstraints() {
+
+        NSLayoutConstraint.activate([
+            myTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            myTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            myTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            myTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+}
+
+// MARK: UITextFieldDelegate
+extension ProfileViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+
+
+
+
+
+//    private lazy var profileHeaderView = ProfileHeaderView()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        setupViews()
+//        setConstraints()
+//    }
+//
+//    private func setupViews() {
+//
+//        view.backgroundColor = .lightGray
+//        view.addSubview(profileHeaderView)
+//        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+//        profileHeaderView.layer.borderWidth = 3
+//        profileHeaderView.layer.borderColor = UIColor.systemBlue.cgColor
+//    }
+//}
+//
+//extension ProfileViewController {
+//
+//    private func setConstraints() {
+//
+//        NSLayoutConstraint.activate([
+//            profileHeaderView.widthAnchor.constraint(equalTo: view.widthAnchor),
+//            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)])
+//    }
+//}
---
+++ Navigation_HW_1.3/Navigation_HW_1.3/UIElement/PostTableViewCell.swift
@@ -0,0 +1,112 @@
+//
+//  PostTableViewCell.swift
+//  Navigation_HW_1.3
+//
+//  Created by Dima Shikhalev on 31.07.2022.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    var author = UILabel()
+    var avatarImageView = UIImageView()
+    var descriptionLabel = UILabel()
+    var likesLabel = UILabel()
+    var viewsLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        configure()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        author.text = nil
+        avatarImageView.image = nil
+        descriptionLabel.text = nil
+        likesLabel.text = nil
+        viewsLabel.text = nil
+    }
+    
+    private func setupViews() {
+        
+        self.addSubview(author)
+        self.addSubview(avatarImageView)
+        self.addSubview(descriptionLabel)
+        self.addSubview(likesLabel)
+        self.addSubview(viewsLabel)
+    }
+    
+    private func configure() {
+    
+        //header
+        author.font = .systemFont(ofSize: 20, weight: .bold)
+        author.textColor = .black
+        author.numberOfLines = 2
+        author.textAlignment = .left
+        author.translatesAutoresizingMaskIntoConstraints = false
+        
+        //image
+        avatarImageView.contentMode = .scaleAspectFit
+        avatarImageView.backgroundColor = .black
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //description
+        descriptionLabel.font = .systemFont(ofSize: 14)
+        descriptionLabel.textColor = .systemGray
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // likes and views
+        likesLabel.font = .systemFont(ofSize: 16)
+        likesLabel.textColor = .black
+        likesLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        viewsLabel.font = .systemFont(ofSize: 16)
+        viewsLabel.textColor = .black
+        viewsLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            author.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+            author.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            
+            avatarImageView.topAnchor.constraint(equalTo: author.bottomAnchor, constant: 8),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            avatarImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            avatarImageView.widthAnchor.constraint(equalTo: self.widthAnchor),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor, multiplier: 1),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            
+            likesLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+            likesLabel.leadingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor),
+            likesLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16),
+            
+            viewsLabel.topAnchor.constraint(equalTo: likesLabel.topAnchor),
+            viewsLabel.trailingAnchor.constraint(equalTo: descriptionLabel.trailingAnchor),
+            viewsLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16)
+        ])
+    }
+    
+    func configureViews(authorNew: String, avatar: String, description: String, likes: Int, views: Int) {
+        author.text = authorNew
+        avatarImageView.image = UIImage(named: avatar)
+        descriptionLabel.text = description
+        likesLabel.text = "Likes: \(likes)"
+        viewsLabel.text = "Views: \(views)"
+    }
+}
